266e705227c1b10c7175c32267c5198dd86fe580
DiverBro <serginar@ucm.es>
2024-03-03 12:27:18 +0100

    Archivos iniciales añadidos al índice del repositorio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prueba_Map_Visual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
